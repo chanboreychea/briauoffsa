c2e288ee1a4edf2106b45b02fd01a46ae2f502a2
borey chea <cheaborey25@gmail.com>
2024-10-10 10:21:06 +0700

    rm authController

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -8,39 +8,39 @@ use Illuminate\Support\Facades\Redirect;
 
 class Auth extends Controller
 {
-    // public function login(Request $request)
-    // {
-    //     $request->validate([
-    //         'username' => 'required',
-    //         'password' => 'required'
-    //     ], [
-    //         'username.required' => 'Please input the username',
-    //         'password.required' => 'Please input the password'
-    //     ]);
+    public function logins(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ], [
+            'username.required' => 'Please input the username',
+            'password.required' => 'Please input the password'
+        ]);
 
-    //     $username = $request->input('username');
-    //     $password = $request->input('password');
+        $username = $request->input('username');
+        $password = $request->input('password');
 
-    //     if ($username == 'admin.iauoffsa.gov.kh' && $password == "123") {
-    //         $request->session()->pull('is_admin_logged_in');
-    //         $request->session()->pull('admin_id');
-    //         session(['is_admin_logged_in' => true, 'admin_id' => "B0r3y!19"]);
-    //         return redirect('/booking');
-    //     }
+        if ($username == 'admin.iauoffsa.gov.kh' && $password == "123") {
+            $request->session()->pull('is_admin_logged_in');
+            $request->session()->pull('admin_id');
+            session(['is_admin_logged_in' => true, 'admin_id' => "B0r3y!19"]);
+            return redirect('/booking');
+        }
 
-    //     return Redirect::route('admin-login');
-    // }
+        return Redirect::route('admin-login');
+    }
 
-    // public function logout(Request $request)
-    // {
+    public function logouts(Request $request)
+    {
 
-    //     if ($request->session()->has('is_admin_logged_in')) {
-    //         $request->session()->pull('is_admin_logged_in');
-    //         $request->session()->pull('admin_id');
-    //     }
+        if ($request->session()->has('is_admin_logged_in')) {
+            $request->session()->pull('is_admin_logged_in');
+            $request->session()->pull('admin_id');
+        }
 
-    //     return Redirect::route('homepage');
-    // }
+        return Redirect::route('homepage');
+    }
 
     public function login(Request $request)
     {
@@ -121,4 +121,3 @@ class Auth extends Controller
         }
     }
 }
-
